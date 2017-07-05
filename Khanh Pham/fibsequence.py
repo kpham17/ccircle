12e9e57c5d427ef65bbfeff3b9c9efe7720aa9b5
kpham17 <khanhpham2157@gmail.com>
2017-07-05 13:29:04 -0500

    Merge branch 'master' of https://github.com/mathcircle/ccircle into Khanh

--- Khanh Pham/fibsequence.py
+++ Khanh Pham/fibsequence.py
@@ -1,8 +1,20 @@
 from math import *
 
 a = int(input("Enter number: "))
-n1 = 0
-n2 = 1
-n3 = 1
-for x in range(0,a)
-    n1 = n2
+def fibN(a):
+    n1 = 0
+    n2 = 1
+    n3 = 1
+    for x in range(0, a - 1):
+        if x == 0 or x == 1:
+            print(1)
+        else:
+            n1 = n2 + n3
+            n2 = n3
+            n3 = n1
+            print(n3)
+
+
+print(fibN(a))
+
+
