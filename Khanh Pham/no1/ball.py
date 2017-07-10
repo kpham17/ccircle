697c82510e64d343f3dd9dc740285009de43339a
Julian <roveejulian@gmail.com>
2017-07-10 16:30:11 -0500

    add ball.py

--- Khanh Pham/no1/ball.py
+++ Khanh Pham/no1/ball.py
@@ -1,8 +1,11 @@
+import math
+
+
 class Ball:
     def __init__(self,x,y):
         self.x = x
         self.y = y
-        self.vx = 200
+        self.vx = 0
         self.vy = 0
         self.fx = 0
         self.fy = 0
@@ -11,11 +14,12 @@ class Ball:
         self.fx+=fx
         self.fy+=fy
 
+
     def draw(self,window):
 
         window.drawCircle(self.x,self.y,16,1,0,0.2,1)
 
-    def update(self,dt):
+    def update(self,dt,x,y):
         accelX = self.fx/self.mass
         accelY = self.fy / self.mass
         self.vx += dt*accelX
@@ -24,13 +28,24 @@ class Ball:
         self.y += dt * self.vy
         self.fx =0
         self.fy = 0
-        if self.y > 500:
-            self.vy*=-0.99
+        a = self.x - x
+        b = self.y - y
+        if a == 0:
+            self.vx *= -1.2
+        elif 0 < abs(a) <= 2:
+            self.vx *= -2 / (2*a)
+        if b == 0:
+            self.vy *= -1.2
+        elif 0 < abs(b) <= 2:
+            self.vy *= -2 / (2*b)
+        if self.y > 500 or self.y < 0:
+            self.vy *= -0.99
             self.y = 500
         if self.x > 800 or self.x < 0:
-            self.vx*=-0.99
-        if self.y == y and self.x == x:
-            self.vy*=-0.99
             self.vx *= -0.99
+
+
+
+
 
 
--- Khanh Pham/no1/d5.py
+++ Khanh Pham/no1/d5.py
@@ -8,13 +8,13 @@ import ball
 
 window = ccircle.Window('lab 5', 800,600)
 myWorld = world.World('d34d')
-
+a = []
 for i in range(200):
     x = random.randint(0,800)
     y = random.randint(0, 150)
     size = random.randint(50,75)
     color = random.uniform(0.5,1)
-    vx = random.randint(0,30)
+    vx = random.randint(-30,30)
     myWorld.add(Sky.Cloud(x,y,size,color,vx))
 
 for i in range(1000):
@@ -23,16 +23,18 @@ for i in range(1000):
     size = random.randint(10,40)
     color = random.uniform(0.5,1)
     myWorld.add(bush.Bush(x,y,size,color))
-
-myball = ball.Ball(400,200)
-myWorld.add(myball)
+for i in range(1):
+    x = random.randint(200, 600)
+    y = random.randint(200, 400)
+    myWorld.add(ball.Ball(x,y))
+    a.append([x,y])
 
 start = time.time()
 dt = 1.0/60.0
 while window.isOpen():
     window.clear(0.8,0.8,0.8)
     x,y = window.getMousePos()
-    myball.apply_force(0,1009.8)
+    my_ball.apply_force(100,1009.8)
     myWorld.draw(window)
     myWorld.update(dt,x,y)
     window.update()
